e047af4a360d0c9e15b8e25cf7e2780376b147d1
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-07-13 19:06:15 +0500

    Tiny layout improvement

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,53 +1,54 @@
 import React from 'react';
-import { Segment, Sidebar, Menu, Header, Icon, Container } from 'semantic-ui-react';
-import "semantic-ui-css/semantic.min.css";
+import { Segment, Sidebar, Menu, Header, Icon, Container, Grid } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 
 const BasicLayout: React.FC = props => {
     return (
         <div>
-            <Menu fixed='top'>
+            <Menu>
                 <Menu.Item name='browse'>
                     Browse
-                            </Menu.Item>
-
+                </Menu.Item>
                 <Menu.Item name='submit'>
                     Submit
-                            </Menu.Item>
+                </Menu.Item>
                 <Menu.Menu position='right'>
                     <Menu.Item name='signup'>
                         Sign Up
-                                </Menu.Item>
+                    </Menu.Item>
                     <Menu.Item>
                         Help
-                                </Menu.Item>
+                    </Menu.Item>
                 </Menu.Menu>
             </Menu>
-            <Sidebar.Pushable as={Segment}>
-                <Sidebar
-                    as={Menu}
-                    animation='uncover'
-                    icon='labeled'
-                    visible={true}
-                    vertical={true}
-                    width='thin'
-                >
-                    <Menu.Item href='/'>
-                        <Icon name='home' />
-                        Главная
+            <Grid>
+                <Sidebar.Pushable as={Segment}>
+                    <Sidebar
+                        as={Menu}
+                        animation='uncover'
+                        icon='labeled'
+                        visible={true}
+                        vertical={true}
+                        width='thin'
+                    >
+                        <Menu.Item href='/'>
+                            <Icon name='home' />
+                            Главная
                     </Menu.Item>
-                    <Menu.Item href='/manage/pages'>
-                        <Icon name='sitemap' />
-                        Ресурсы
+                        <Menu.Item href='/manage/pages'>
+                            <Icon name='sitemap' />
+                            Ресурсы
                     </Menu.Item>
-                </Sidebar>
-                <Sidebar.Pusher>
-                    <Segment basic={true}>
-                        <Container>
-                            {props.children}
-                        </Container>
-                    </Segment>
-                </Sidebar.Pusher>
-            </Sidebar.Pushable>
+                    </Sidebar>
+                    <Sidebar.Pusher>
+                        <Segment basic={true}>
+                            <Container>
+                                {props.children}
+                            </Container>
+                        </Segment>
+                    </Sidebar.Pusher>
+                </Sidebar.Pushable>
+            </Grid>
         </div>
     );
 };
