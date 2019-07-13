2f84442471fc58a938caa88bc015d516c7614f67
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-07-13 18:14:41 +0500

    Add new "manage pages" page

---
+++ src/pages/manage/pages.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+import styles from './pages.css';
+
+export default function() {
+  return (
+    <div className={styles.normal}>
+      <h1>Page pages</h1>
+    </div>
+  );
+}
