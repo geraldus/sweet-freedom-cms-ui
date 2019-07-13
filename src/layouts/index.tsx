79672d299d011f2793280e9a0ed51664df55a995
Arthur Sakhievich Fayzrakhmanov <heraldhoi@gmail.com>
2019-07-13 16:19:02 +0500

    Implement sidebar

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import styles from './index.css';
+import { Segment, Sidebar, Menu, Header, Icon } from 'semantic-ui-react';
+import "semantic-ui-css/semantic.min.css";
 
 const BasicLayout: React.FC = props => {
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-      {props.children}
-    </div>
-  );
+    return (
+        <div>
+            <Sidebar.Pushable as={Segment}>
+                <Sidebar
+                    as={Menu}
+                    animation='uncover'
+                    visible={true}
+                    vertical={true}
+                    width='thin'
+                >
+                    <Menu.Item>Ресурсы</Menu.Item>
+                    <Menu.Item>Test 3</Menu.Item>
+                </Sidebar>
+                <Sidebar.Pusher>
+                    <Segment basic={true}>
+                        <h1>Yay! Welcome to umi!</h1>
+                        {props.children}
+                    </Segment>
+                </Sidebar.Pusher>
+            </Sidebar.Pushable>
+        </div>
+    );
 };
 
 export default BasicLayout;
