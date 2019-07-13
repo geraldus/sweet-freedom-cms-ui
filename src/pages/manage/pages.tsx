500a4f0810b20cba9a303eb34b10dabb55dd7c81
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-07-13 19:06:29 +0500

    Mock sitemap list view

--- src/pages/manage/pages.tsx
+++ src/pages/manage/pages.tsx
@@ -1,12 +1,37 @@
-
 import React from 'react';
+import { formatMessage } from 'umi-plugin-locale';
 
-import styles from './pages.css';
+import { Header, Divider, Icon, List } from 'semantic-ui-react';
 
-export default function() {
-  return (
-    <div className={styles.normal}>
-      <h1>Page pages</h1>
-    </div>
-  );
+export default function () {
+    return (
+        <div>
+            <Header>{formatMessage({ id: 'manage.pages.pageTitle' })}</Header>
+            <Divider/>
+            <List size='big' selection={true} relaxed={true}>
+                <List.Item>
+                    <List.Icon name='sitemap' />
+                    <List.Content>
+                        <List.Header>Сайт</List.Header>
+                    </List.Content>
+                    <List.List>
+                        <List.Item>
+                            <List.Icon name='file' />
+                            <List.Content>
+                                <List.Header>Главная</List.Header>
+                                <List.Description>/</List.Description>
+                            </List.Content>
+                        </List.Item>
+                        <List.Item>
+                            <List.Icon name='file' />
+                            <List.Content>
+                                <List.Header>Новости</List.Header>
+                                <List.Description>/news</List.Description>
+                            </List.Content>
+                        </List.Item>
+                    </List.List>
+                </List.Item>
+            </List>
+        </div>
+    );
 }
