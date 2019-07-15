1e87ab3805d95f8ea17d092f8cc931167edb0e73
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-07-15 19:09:24 +0500

    Add translations

--- src/locales/en-US.ts
+++ src/locales/en-US.ts
@@ -1,4 +1,5 @@
 export default {
     'index.start': 'Getting Started',
-    'manage.pages.pageTitle': 'Manage Site Pages'
+    'manage.pages.pageTitle': 'Manage Site Pages',
+    'manage.siteMap.root': 'Root'
 }
--- src/locales/ru-RU.ts
+++ src/locales/ru-RU.ts
@@ -1,4 +1,5 @@
 export default {
     'index.start': 'Приступая к работе',
-    'manage.pages.pageTitle': 'Страницы сайта'
+    'manage.pages.pageTitle': 'Страницы сайта',
+    'manage.siteMap.root': 'Сайт'
 }
